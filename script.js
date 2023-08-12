64af47355473f77e0a4e3bf0a7789a14c9fa64b4
Potency <proper@ProperPoe>
2023-08-12 03:23:00 -0600

    Added a reset button and a newgrid button for user to set new dimensions, also added different logic on the createGrid function
    I added inline styling to the container element, for both width and height, in the creatGrid function. I created a maxBoxSize in the global scope, and boxWidth and boxHeight in the function scope--and use them to add to the gridTemplateRows and gridTemplateColumns--so the box sizes adjust to the different dimenions. I also added min-height and min-width to the container-contain selector, so the grid never shrinks or increases. I added a reset button that removes the active class from the boxes, and new grid button that renders an innterHTML form when clicked. This form lets the user enter width and height to adjust the grids dimensions

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
 const square = document.getElementById("square");
 const container = document.getElementById("container");
 const reset = document.getElementById("reset");
+const input = document.getElementById("input");
+const newBtn = document.getElementById("new")
+const maxBoxSize = 700;
+
+const initialWidth = 16;
+const initalHeight = 16;
 
 
-for(let i = 0; i < 64; i++){
-    for(let j = 0; j < 64; j++){
-        let newOne = document.createElement("div");
-        newOne.classList.add("box");
 
-        newOne.addEventListener("mouseover", function() {
-            this.classList.add("active")
-        })
+function createGrid (width, height){
+    container.innerHTML = "";
+
+    const boxWidth = Math.min(maxBoxSize, 700/width);
+    const boxHeight = Math.min(maxBoxSize, 700/height);
+
+    container.style.gridTemplateRows = `repeat(${height}, ${boxHeight}px)`;
+    container.style.gridTemplateColumns = `repeat(${width}, ${boxWidth}px)`;
+    for(let i = 0; i < height; i++){
+        for(let j = 0; j < width; j++){
+            let newOne = document.createElement("div");
+            newOne.classList.add("box");
+    
+            newOne.addEventListener("mouseover", function() {
+                this.classList.add("active");
+            })
+            
+            reset.addEventListener("click", function() {
+                newOne.classList.remove("active");
+            })
+            container.appendChild(newOne);      
+        }
         
-        reset.addEventListener("click", function() {
-            newOne.classList.remove("active")
-        })
-
-        container.appendChild(newOne)
-
         
     }
-    
-    
 }
 
-/*for(let i = 0; i < 7; i++){
-    for(x = 0; x < 7; x++){
-        console.log('#')
-    }
-}*/
+createGrid(initialWidth, initalHeight)
+
+newBtn.addEventListener("click", () => {
+    input.innerHTML = `<div>
+        <input class="height" id="height" type="text">
+        <input class="width" id="width" type="text">
+        <button id="createBtn">Create</button>
+        </div>`
+    
+    const createBtn = document.getElementById("createBtn");
+    const gridHeight = document.getElementById("height")
+    const gridWidth = document.getElementById("width")
+    createBtn.addEventListener("click", () => {
+        let width = gridWidth.value;
+        let height = gridHeight.value;
+        
+        if(!isNaN(width) && !isNaN(height) && width > 0 && height > 0){
+            createGrid(width, height);
+        }
+        input.innerHTML = "";
+    })
+})
+
 
